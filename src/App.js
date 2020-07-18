0e207235cdebbc4e7776153dea26b5621deb189d
egor <eegor650@gmail.com>
2020-07-17 18:32:51 -0700

    make books show up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  function updateSearchValue(event) {
+    setSearchValue(event.target.value)
+  }
+
+  function sendSearchQuery() {
+    const apiKey = "AIzaSyBTAujqW8lRdVFUo_SJiaTZ79zh3lTa7FA";
+    const apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchValue + "&key=" + apiKey;
+    fetch(apiUrl).then(res => res.json()).then((json) => {
+      setSearchResults(json.items);
+      console.log(json.items)
+    })
+  }
   return (
     <div>
       <input
-        placeholder="search book"
-        onChange={(event) => {
-          console.log(event.target.value)
-        }}
+        value={searchValue}
+        placeholder="search books"
+        onChange={updateSearchValue}
       /> 
-      <button>Find</button>
+      <button onClick={sendSearchQuery}>Find</button>
+      <div>
+        {searchResults
+        .map(item => (
+          <div>
+            <img src={item.volumeInfo.imageLinks.thumbnail}/>
+            <div>
+              {item.volumeInfo.title} by {item.volumeInfo.authors[0]}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
